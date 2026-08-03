4418b69cb03a8d2d97a403edece37cad0e9171d0
Sanjana Jaiswal <sanjanajaiswalbst@gmail.com>
2026-08-03 15:43:42 +0530

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        #The Binary search solution
+        left,right=1,max(piles)
+        result=right
+        while left<=right:
+            mid=(left+right)//2
+            totalTime=0
+            for p in piles:
+                totalTime+=math.ceil(p/mid)
+            if totalTime <= h:
+                result = mid
+                right=mid-1
+            else:
+                left=mid+1
+        return result
+        
